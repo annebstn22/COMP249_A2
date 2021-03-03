d52f96426b4cf711368a920eedfdb61ecf72b325
Anne <annebstn22@hotmail.ca>
2021-03-03 16:48:54 -0500

    commented out to run driver

--- traceObsoleteAddress.java
+++ traceObsoleteAddress.java
@@ -1,10 +1,10 @@
-import java.Package1.Address;
+import Package1.Address;
 
 public abstract class traceObsoleteAddress {
-    public abstract String traceObsoleteAddress(Address[] arr, int year, int month, int day) {
+    /* public abstract String traceObsoleteAddress(Address[] arr, int year, int month, int day) {
         String date = Integer.toString(year) + "-" + Integer.toString(month) + "-" + Integer.toString(day);
         boolean obsolete = true;
-        for (int i=0; i<arr.length; i++){
+        for (int i=0; i<arr.length; i++){ */
             /*int startYear = Integer.parseInt(arr[i].validFrom.substring(0,3));
             int startMonth = Integer.parseInt(arr[i].validFrom.substring(5,7));
             int startDay = Integer.parseInt(arr[i].validFrom.substring(7,9));
@@ -21,9 +21,9 @@ public abstract class traceObsoleteAddress {
                 }
             }*/
 
-            obsolete = (arr[i].validFrom.compareTo(date)<=0 && arr[i].validTo.compareTo(date)>=0)? false:true;
-            return(obsolete? arr[i] + " is obsolete." : arr[i] + " is valid.");
-        }
+            /*obsolete = (arr[i].validFrom.compareTo(date)<=0 && arr[i].validTo.compareTo(date)>=0)? false:true;
+            return(obsolete? arr[i] + " is obsolete." : arr[i] + " is valid.");*/
+       /* }
         
-    }
+    }*/
 }

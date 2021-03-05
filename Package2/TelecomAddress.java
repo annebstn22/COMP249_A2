ef9927179d8654b3fa6948dad26873c67d5877db
Anne <annebstn22@hotmail.ca>
2021-03-05 18:46:53 -0500

    change access mod attr, add display(), param name

--- Package2/TelecomAddress.java
+++ Package2/TelecomAddress.java
@@ -3,25 +3,26 @@ package Package2;
 import Package1.Address;
 
 public class TelecomAddress extends Address {
-    protected int countryCode;
-    protected String nationalDialingPrefix;
-    protected int areaCode;
-    protected long number;
-    protected int extension;
-    protected String physicalType;
+    private int countryCode;
+    private String nationalDialingPrefix;
+    private int areaCode;
+    private long number;
+    private int extension;
+    private String physicalType;
 
+    //Default Constructor
     public TelecomAddress() {
-        // super();
         countryCode = 0;
-        nationalDialingPrefix = "";
+        nationalDialingPrefix = "(0)";
         areaCode = 0;
-        number = 0;
-        extension = 0;
-        physicalType = "";
+        number = 00000000;
+        extension = 000;
+        physicalType = "type-unknown";
     }
 
-    public TelecomAddress(String validTo, String validFrom, int countryCode, String nationalDialingPrefix, int areaCode, long number, int extension, String physicalType) {
-        super(validTo, validFrom);
+    //Parameterized Constructor
+    public TelecomAddress(String validFrom, String validTo, int countryCode, String nationalDialingPrefix, int areaCode, long number, int extension, String physicalType) {
+        super(validFrom, validTo);
         this.countryCode =  countryCode;
         this.nationalDialingPrefix = nationalDialingPrefix;
         this.areaCode = areaCode;
@@ -30,36 +31,40 @@ public class TelecomAddress extends Address {
         this.physicalType = physicalType;
     }
 
+    //Copy Constructor
     public TelecomAddress(TelecomAddress an_address) {
-        this(an_address.validTo, an_address.validFrom, an_address.countryCode, an_address.nationalDialingPrefix, an_address.areaCode, an_address.number, an_address.extension, an_address.physicalType);
+        this(an_address.validFrom, an_address.validTo, an_address.countryCode, an_address.nationalDialingPrefix, an_address.areaCode, an_address.number, an_address.extension, an_address.physicalType);
     }
 
     
     /** 
-     * @param otherObject
      * @return String
      */
     public String toString(){ 
-        return ("This telecom address +" + countryCode + " " + nationalDialingPrefix + areaCode + " " + number + " ext. " + extension + " " + physicalType + " is valid from " + validFrom + " to " + validTo + ".");
+        return ("The TELECOM ADDRESS:\n" + countryCode + " " + nationalDialingPrefix + areaCode + " " + number + " ext. " + extension + " " + physicalType + " is valid from " + validFrom + " to " + validTo);
     }
 
+    public String display(){
+        return (countryCode + " " + nationalDialingPrefix + areaCode + " " + number + " ext. " + extension + " " + physicalType);
+    }
     
     /** 
      * @param otherObject
      * @return boolean
      */
-    public boolean equals(Address otherObject) {
+    public boolean equals(Address otherAddress) {
 
-        if (otherObject == null)
+        if (otherAddress == null){
             return false;
+        }
 
-        String thisClass = getClass().toString(), otherClass = otherObject.getClass().toString(); 
+        String thisClass = getClass().toString(), otherClass = otherAddress.getClass().toString(); 
 
         if (!thisClass.substring(6).equals(otherClass.substring(6))) 
             return false;
         else {
-            TelecomAddress otherTelecomAddress = (TelecomAddress)otherObject;
-            return (validTo.equals(otherTelecomAddress.validTo) && validFrom.equals(otherTelecomAddress.validFrom) && countryCode == otherTelecomAddress.countryCode && nationalDialingPrefix.equals(otherTelecomAddress.nationalDialingPrefix) && areaCode == otherTelecomAddress.areaCode && number == otherTelecomAddress.number && extension == otherTelecomAddress.extension && physicalType == otherTelecomAddress.physicalType);
+            TelecomAddress otherTelecomAddress = (TelecomAddress)otherAddress;
+            return (validTo.equals(otherTelecomAddress.validTo) && validFrom.equals(otherTelecomAddress.validFrom) && countryCode == otherTelecomAddress.countryCode && nationalDialingPrefix.equals(otherTelecomAddress.nationalDialingPrefix) && areaCode == otherTelecomAddress.areaCode && number == otherTelecomAddress.number && extension == otherTelecomAddress.extension && physicalType.equals(otherTelecomAddress.physicalType));
         }
     }
 }

867c92e20459f4cca3780a980e41f24f04dc36cb
Anne <annebstn22@hotmail.ca>
2021-03-05 18:48:23 -0500

    changed access mod, add access+mod

--- Package3/GeographicAddress.java
+++ Package3/GeographicAddress.java
@@ -5,23 +5,24 @@ import Locale.Locale;
 import Package1.Address;
 
 public class GeographicAddress extends Address {
-    private String addressLine;
-    private String city;
-    private String regionOrState;
-    private String zipOrPostCode;
-    private Locale loc;
+    protected String addressLine; //DOUBLE CHECK: protected so that PostOfficeBoxAddress can have access
+    protected String city;
+    protected String regionOrState;
+    protected String zipOrPostCode;
+    protected Locale loc;
 
+    //Default Constructor
     public GeographicAddress() {
-        // super();
-        addressLine = "";
-        city = "";
-        regionOrState = "";
-        zipOrPostCode = "";
+        addressLine = "address";
+        city = "city";
+        regionOrState = "region/state";
+        zipOrPostCode = "zip/postcode";
         loc = new Locale();
     }
 
-    public GeographicAddress(String validTo, String validFrom, String addressLine, String city, String regionOrState, String zipOrPostCode, Locale loc1) {
-        super(validTo, validFrom);
+    //Parameterized Constructor
+    public GeographicAddress(String validFrom, String validTo, String addressLine, String city, String regionOrState, String zipOrPostCode, Locale loc1) {
+        super(validFrom, validTo);
         this.addressLine = addressLine;
         this.city = city;
         this.regionOrState = regionOrState;
@@ -29,34 +30,83 @@ public class GeographicAddress extends Address {
         loc = new Locale(loc1);
     }
 
+    //Copy Constructor
     public GeographicAddress(GeographicAddress someGeoAddress) {
-        this(someGeoAddress.validTo, someGeoAddress.validFrom, someGeoAddress.addressLine, someGeoAddress.city, someGeoAddress.regionOrState, someGeoAddress.zipOrPostCode, someGeoAddress.loc);
+        this(someGeoAddress.validFrom, someGeoAddress.validTo, someGeoAddress.addressLine, someGeoAddress.city, someGeoAddress.regionOrState, someGeoAddress.zipOrPostCode, someGeoAddress.loc);
     }
 
-    
+    // Accessors and Mutators
+
+    public String getAddressLine(){
+        return addressLine;
+    }
+
+    public String getCity(){
+        return city;
+    }
+
+    public String getRegionOrState(){
+        return regionOrState;
+    }
+
+    public String getZipOrPostCode(){
+        return zipOrPostCode;
+    }
+
+    public Locale getLocale(){
+        return new Locale(this.loc);
+    }
+
+    public void setAddressLine(String addressLine){
+        this.addressLine = addressLine;
+    }
+
+    public void setCity(String city){
+        this.city = city;
+    }
+
+    public void setRegionOrState(String regionOrState){
+        this.regionOrState = regionOrState;
+    }
+
+    public void setZipOrPostCode(String zipOrPostCode){
+        this.zipOrPostCode = zipOrPostCode;
+    }
+
+    public void setLocale(Locale loc){
+        Locale newLoc = new Locale(loc);
+        this.loc = newLoc;
+    }
+
+
     /**
      * @return String
      */
-    public String toString() {  //fix to correct formatting later
-        return (addressLine + ", " + city + ", " + regionOrState + ".\n" + zipOrPostCode + "\n" + loc);
+    public String toString() { 
+        return ("The GEOGRAPHIC ADDRESS:\n" + addressLine + ", " + city + ", " + regionOrState + zipOrPostCode + loc + " \nis valid from " + validFrom + " to " + validTo);
     }
     
     
     /** 
-     * @param otherObject
+     * @param otherAddress
      * @return boolean
      */
-    public boolean equals(Address otherObject) {
-        if (otherObject == null)
-        return false;
+    public boolean equals(Address otherAddress) {
 
-        String thisClass = getClass().toString(), otherClass = otherObject.getClass().toString(); 
+        if (otherAddress == null){
+            return false;
+        }
+
+        String thisClass = getClass().toString(), otherClass = otherAddress.getClass().toString(); 
 
         if (!thisClass.substring(6).equals(otherClass.substring(6))) 
             return false;
         else {
-            GeographicAddress otherGeographicAddress = (GeographicAddress)otherObject;
-            return (validTo.equals(otherGeographicAddress.validTo) && validFrom.equals(otherGeographicAddress.validFrom) && addressLine.equals(otherGeographicAddress.addressLine) && city.equals(otherGeographicAddress.city) && city.equals(otherGeographicAddress.city) && zipOrPostCode.equals(otherGeographicAddress.zipOrPostCode) && loc.equals(otherGeographicAddress.loc));
+            GeographicAddress otherGeographicAddress = (GeographicAddress)otherAddress;
+            return (validTo.equals(otherGeographicAddress.validTo) && validFrom.equals(otherGeographicAddress.validFrom) 
+            && addressLine.equals(otherGeographicAddress.addressLine) && city.equals(otherGeographicAddress.city) 
+            && zipOrPostCode.equals(otherGeographicAddress.zipOrPostCode) && regionOrState.equals(otherGeographicAddress.regionOrState) 
+            && loc.equals(otherGeographicAddress.loc));
         }
     }
 }
